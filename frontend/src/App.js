991576c88444d9d85576b25be1260d0e000fe520
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2021-11-30 23:29:11 -0700

    added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import LoginForm from './Components//Login/LoginForm';
 import SignUpForm from './Components//SignUp/SignUpForm';
 import { BrowserRouter as Router, Switch, Route, Redirect  } from 'react-router-dom';
-import Navigation from './Components/PublicNavigation/Navigation.jsx'
 import Home from './Components/Pages/Home.jsx';
 import Feed from './Components/Pages/Profile';
 import userProfile from './Components/Pages/UserProfile';
@@ -14,6 +13,7 @@ import UserProfile from './Components/Pages/UserProfile';
 import Messages from './Components/Pages/Messages/Messages';
 import SignUpSuccess from './Components/SignUp/Success';
 import CreateProfile from './Components/CreateProfile/CreateProfile';
+import Navigation from './Components/PublicNavigation/Navigation.jsx'
 
 
 function App() {
@@ -22,7 +22,8 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/">
-          {user ? <Home /> : <SignUpForm />}
+          <Navigation />
+          <Home />
         </Route>
         <Route path="/login">{user ? <Redirect to="/createprofile" /> : <LoginForm />}</Route>
         <Route path="/signup">
